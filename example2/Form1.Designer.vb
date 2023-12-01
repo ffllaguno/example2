59781804919385db92af9e69efd1de5ec8d23eec
Royce Arcayna <Royce Arcayna@LAPTOP-5IGSOOS4>
2023-12-01 22:27:51 +0800

    biatch

--- example2/Form1.Designer.vb
+++ example2/Form1.Designer.vb
@@ -39,123 +39,123 @@ Partial Class Form1
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(47, 236)
-        Button1.Margin = New Padding(2, 2, 2, 2)
+        Button1.Location = New Point(54, 315)
+        Button1.Margin = New Padding(2, 3, 2, 3)
         Button1.Name = "Button1"
-        Button1.Size = New Size(73, 21)
+        Button1.Size = New Size(83, 28)
         Button1.TabIndex = 0
         Button1.Text = "Button1"
         Button1.UseVisualStyleBackColor = True
         ' 
         ' TextBox1
         ' 
-        TextBox1.Location = New Point(292, 69)
-        TextBox1.Margin = New Padding(2, 2, 2, 2)
+        TextBox1.Location = New Point(334, 92)
+        TextBox1.Margin = New Padding(2, 3, 2, 3)
         TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(98, 23)
+        TextBox1.Size = New Size(111, 27)
         TextBox1.TabIndex = 1
         ' 
         ' Button2
         ' 
-        Button2.Location = New Point(518, 113)
-        Button2.Margin = New Padding(2, 2, 2, 2)
+        Button2.Location = New Point(592, 151)
+        Button2.Margin = New Padding(2, 3, 2, 3)
         Button2.Name = "Button2"
-        Button2.Size = New Size(73, 21)
+        Button2.Size = New Size(83, 28)
         Button2.TabIndex = 2
         Button2.Text = "Button2"
         Button2.UseVisualStyleBackColor = True
         ' 
         ' Button3
         ' 
-        Button3.Location = New Point(454, 258)
-        Button3.Margin = New Padding(2, 2, 2, 2)
+        Button3.Location = New Point(519, 344)
+        Button3.Margin = New Padding(2, 3, 2, 3)
         Button3.Name = "Button3"
-        Button3.Size = New Size(73, 21)
+        Button3.Size = New Size(83, 28)
         Button3.TabIndex = 3
         Button3.Text = "Button3"
         Button3.UseVisualStyleBackColor = True
         ' 
         ' Button4
         ' 
-        Button4.Location = New Point(74, 71)
-        Button4.Margin = New Padding(2, 2, 2, 2)
+        Button4.Location = New Point(85, 95)
+        Button4.Margin = New Padding(2, 3, 2, 3)
         Button4.Name = "Button4"
-        Button4.Size = New Size(73, 21)
+        Button4.Size = New Size(83, 28)
         Button4.TabIndex = 4
         Button4.Text = "Button4"
         Button4.UseVisualStyleBackColor = True
         ' 
         ' TextBox2
         ' 
-        TextBox2.Location = New Point(265, 138)
-        TextBox2.Margin = New Padding(2)
+        TextBox2.Location = New Point(303, 184)
+        TextBox2.Margin = New Padding(2, 3, 2, 3)
         TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(98, 23)
+        TextBox2.Size = New Size(111, 27)
         TextBox2.TabIndex = 1
         ' 
         ' TextBox3
         ' 
-        TextBox3.Location = New Point(350, 207)
-        TextBox3.Margin = New Padding(2)
+        TextBox3.Location = New Point(400, 276)
+        TextBox3.Margin = New Padding(2, 3, 2, 3)
         TextBox3.Name = "TextBox3"
-        TextBox3.Size = New Size(98, 23)
+        TextBox3.Size = New Size(111, 27)
         TextBox3.TabIndex = 1
         ' 
         ' TextBox4
         ' 
-        TextBox4.Location = New Point(101, 149)
-        TextBox4.Margin = New Padding(2)
+        TextBox4.Location = New Point(115, 199)
+        TextBox4.Margin = New Padding(2, 3, 2, 3)
         TextBox4.Name = "TextBox4"
-        TextBox4.Size = New Size(98, 23)
+        TextBox4.Size = New Size(111, 27)
         TextBox4.TabIndex = 1
         ' 
         ' TextBox5
         ' 
-        TextBox5.Location = New Point(491, 0)
-        TextBox5.Margin = New Padding(2)
+        TextBox5.Location = New Point(561, 0)
+        TextBox5.Margin = New Padding(2, 3, 2, 3)
         TextBox5.Name = "TextBox5"
-        TextBox5.Size = New Size(98, 23)
+        TextBox5.Size = New Size(111, 27)
         TextBox5.TabIndex = 1
         ' 
         ' TextBox6
         ' 
-        TextBox6.Location = New Point(327, 11)
-        TextBox6.Margin = New Padding(2)
+        TextBox6.Location = New Point(374, 15)
+        TextBox6.Margin = New Padding(2, 3, 2, 3)
         TextBox6.Name = "TextBox6"
-        TextBox6.Size = New Size(98, 23)
+        TextBox6.Size = New Size(111, 27)
         TextBox6.TabIndex = 1
         ' 
         ' TextBox7
         ' 
-        TextBox7.Location = New Point(191, 220)
-        TextBox7.Margin = New Padding(2)
+        TextBox7.Location = New Point(218, 293)
+        TextBox7.Margin = New Padding(2, 3, 2, 3)
         TextBox7.Name = "TextBox7"
-        TextBox7.Size = New Size(98, 23)
+        TextBox7.Size = New Size(111, 27)
         TextBox7.TabIndex = 1
         ' 
         ' TextBox8
         ' 
-        TextBox8.Location = New Point(169, 276)
-        TextBox8.Margin = New Padding(2)
+        TextBox8.Location = New Point(193, 368)
+        TextBox8.Margin = New Padding(2, 3, 2, 3)
         TextBox8.Name = "TextBox8"
-        TextBox8.Size = New Size(98, 23)
+        TextBox8.Size = New Size(111, 27)
         TextBox8.TabIndex = 1
         ' 
         ' Button5
         ' 
-        Button5.Location = New Point(496, 169)
-        Button5.Margin = New Padding(2)
+        Button5.Location = New Point(567, 225)
+        Button5.Margin = New Padding(2, 3, 2, 3)
         Button5.Name = "Button5"
-        Button5.Size = New Size(73, 21)
+        Button5.Size = New Size(83, 28)
         Button5.TabIndex = 2
         Button5.Text = "Button2"
         Button5.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(622, 321)
+        ClientSize = New Size(1267, 651)
         Controls.Add(Button4)
         Controls.Add(Button3)
         Controls.Add(Button5)
@@ -169,7 +169,7 @@ Partial Class Form1
         Controls.Add(TextBox2)
         Controls.Add(TextBox1)
         Controls.Add(Button1)
-        Margin = New Padding(2, 2, 2, 2)
+        Margin = New Padding(2, 3, 2, 3)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
