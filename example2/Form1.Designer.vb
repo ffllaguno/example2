ea06cd31a0631a507a645658af2afb597ff78965
JAMES JAMES <jamesclaveria16@gmail.com>
2023-12-01 21:49:41 +0800

    YAWA

--- example2/Form1.Designer.vb
+++ example2/Form1.Designer.vb
@@ -27,61 +27,149 @@ Partial Class Form1
         Button2 = New Button()
         Button3 = New Button()
         Button4 = New Button()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        TextBox7 = New TextBox()
+        TextBox8 = New TextBox()
+        Button5 = New Button()
         SuspendLayout()
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(343, 257)
+        Button1.Location = New Point(47, 236)
+        Button1.Margin = New Padding(2, 2, 2, 2)
         Button1.Name = "Button1"
-        Button1.Size = New Size(94, 29)
+        Button1.Size = New Size(73, 21)
         Button1.TabIndex = 0
         Button1.Text = "Button1"
         Button1.UseVisualStyleBackColor = True
         ' 
         ' TextBox1
         ' 
-        TextBox1.Location = New Point(375, 96)
+        TextBox1.Location = New Point(292, 69)
+        TextBox1.Margin = New Padding(2, 2, 2, 2)
         TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(125, 29)
+        TextBox1.Size = New Size(98, 23)
         TextBox1.TabIndex = 1
         ' 
         ' Button2
         ' 
-        Button2.Location = New Point(442, 195)
+        Button2.Location = New Point(518, 113)
+        Button2.Margin = New Padding(2, 2, 2, 2)
         Button2.Name = "Button2"
-        Button2.Size = New Size(94, 29)
+        Button2.Size = New Size(73, 21)
         Button2.TabIndex = 2
         Button2.Text = "Button2"
         Button2.UseVisualStyleBackColor = True
         ' 
         ' Button3
         ' 
-        Button3.Location = New Point(486, 289)
+        Button3.Location = New Point(454, 258)
+        Button3.Margin = New Padding(2, 2, 2, 2)
         Button3.Name = "Button3"
-        Button3.Size = New Size(94, 29)
+        Button3.Size = New Size(73, 21)
         Button3.TabIndex = 3
         Button3.Text = "Button3"
         Button3.UseVisualStyleBackColor = True
         ' 
         ' Button4
         ' 
-        Button4.Location = New Point(230, 186)
+        Button4.Location = New Point(74, 71)
+        Button4.Margin = New Padding(2, 2, 2, 2)
         Button4.Name = "Button4"
-        Button4.Size = New Size(94, 29)
+        Button4.Size = New Size(73, 21)
         Button4.TabIndex = 4
         Button4.Text = "Button4"
         Button4.UseVisualStyleBackColor = True
         ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(265, 138)
+        TextBox2.Margin = New Padding(2)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(98, 23)
+        TextBox2.TabIndex = 1
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(350, 207)
+        TextBox3.Margin = New Padding(2)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(98, 23)
+        TextBox3.TabIndex = 1
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(101, 149)
+        TextBox4.Margin = New Padding(2)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(98, 23)
+        TextBox4.TabIndex = 1
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(491, 0)
+        TextBox5.Margin = New Padding(2)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(98, 23)
+        TextBox5.TabIndex = 1
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(327, 11)
+        TextBox6.Margin = New Padding(2)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(98, 23)
+        TextBox6.TabIndex = 1
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(191, 220)
+        TextBox7.Margin = New Padding(2)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(98, 23)
+        TextBox7.TabIndex = 1
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Location = New Point(169, 276)
+        TextBox8.Margin = New Padding(2)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(98, 23)
+        TextBox8.TabIndex = 1
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(496, 169)
+        Button5.Margin = New Padding(2)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(73, 21)
+        Button5.TabIndex = 2
+        Button5.Text = "Button2"
+        Button5.UseVisualStyleBackColor = True
+        ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(9F, 21F)
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(622, 321)
         Controls.Add(Button4)
         Controls.Add(Button3)
+        Controls.Add(Button5)
         Controls.Add(Button2)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox8)
+        Controls.Add(TextBox7)
+        Controls.Add(TextBox2)
         Controls.Add(TextBox1)
         Controls.Add(Button1)
+        Margin = New Padding(2, 2, 2, 2)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
@@ -93,4 +181,12 @@ Partial Class Form1
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Button5 As Button
 End Class
